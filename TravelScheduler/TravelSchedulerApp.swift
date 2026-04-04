d89f443d88b0458b1369db04763fdf7f607a2c23
POWEHI2023 <zxe6b885@163.com>
2026-04-04 19:48:15 +0800

    Initial Commit

---
+++ TravelScheduler/TravelSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelSchedulerApp.swift
+//  TravelScheduler
+//
+//  Created by zxe6b885 on 2026/4/4.
+//
+
+import SwiftUI
+
+@main
+struct TravelSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
